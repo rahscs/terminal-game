ffc42beb61afc9f9525ef634d9f6f997576f5b71
Jeron Aldaron Lau <deadbeef.jal@gmail.com>
2017-01-19 17:36:18 -0600

    Added context to make it easier to add psuedo-global variables.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,11 @@ enum Direction {
 	Left, Right, Up, Down,
 }
 
+struct Context {
+	world: std::vec::Vec<u8>,
+	stdout: termion::raw::RawTerminal<std::io::Stdout>,
+}
+
 fn backup(pos: &mut (u16, u16), dir: Direction) {
 	match dir {
 		Direction::Left => pos.0 += 1,
@@ -29,71 +34,76 @@ fn backup(pos: &mut (u16, u16), dir: Direction) {
 	}
 }
 
-fn redraw(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>,
-	world: &mut [u8], pos: &mut (u16, u16), dir: Direction, fight: u8)
-	-> bool
+fn redraw(context: &mut Context, pos: &mut (u16, u16), dir: Direction,
+	fight: u8) -> bool
 {
 	let mut found_it = false;
 	let mut floor_msg = "Step ...  Step ...        ";
 	// Clear first line.
 	if fight == 2 {
 		floor_msg = "";
-//		write!(stdout, "{}{}", termion::cursor::Goto(1, 2),
-//			termion::clear::CurrentLine).unwrap();
+		write!(context.stdout, "{}{}", termion::cursor::Goto(1, 2),
+			termion::clear::CurrentLine).unwrap();
+	}else if fight == 3 {
+		write!(context.stdout, "{}{}{}{}", termion::cursor::Goto(1, 1),
+			termion::clear::CurrentLine,termion::cursor::Goto(1, 2),
+			termion::clear::CurrentLine).unwrap();
+		floor_msg = "There is nothing to fight!";
 	}else{
-		write!(stdout, "{}{}", termion::cursor::Goto(1, 1),
+		write!(context.stdout, "{}{}", termion::cursor::Goto(1, 1),
 			termion::clear::CurrentLine).unwrap();
 	}
 	// Go to 3rd line to draw world
-	write!(stdout, "{}", termion::cursor::Goto(1, 3)).unwrap();
+	write!(context.stdout, "{}", termion::cursor::Goto(1, 3)).unwrap();
 	let mut linedown = 2; // Initial y
 	let mut lineright = 1; // Initial x
 	let mut selected : char = ' ';
 	// Draw world
-	for i in 0..world.len() {
-		if world[i] == ('\n' as u8) {
+	for i in 0..context.world.len() {
+		if context.world[i] == ('\n' as u8) {
 			linedown += 1;
 			lineright = 1;
-			write!(stdout, "{}",
+			write!(context.stdout, "{}",
 				termion::cursor::Goto(1, linedown + 1)).unwrap();
 		} else {
 			if pos.0 == lineright && pos.1 == linedown {
 				if fight == 1 {
-					match world[i] as char {
+					match context.world[i] as char {
 						CAT => {
 							found_it = true;
-							world[i]=' ' as u8;
+							context.world[i]=' ' as u8;
 							floor_msg = "kill cat";
 						},
 						EEL => {
 							found_it = true;
-							world[i]=' ' as u8;
+							context.world[i]=' ' as u8;
 							floor_msg = "kill eel";
 						}
 						TREX => {
 							found_it = true;
-							world[i]=' ' as u8;
+							context.world[i]=' ' as u8;
 							floor_msg = "kill trex";
 						}
 						ANTEATER => {
 							found_it = true;
-							world[i]=' ' as u8;
+							context.world[i]=' ' as u8;
 							floor_msg =
 								"kill anteater";
 						}
 						_ => {},
 					}
 				}
-				selected = world[i] as char;
+				selected = context.world[i] as char;
 			}
-			write!(stdout, "{}", world[i] as char);
+			write!(context.stdout, "{}", context.world[i] as char)
+				.unwrap();
 			lineright += 1;
 		}
 	}
 
 	// Write details for block.
 	let stringy = format!("selected : {}", selected);
-	write!(stdout, "{}{}", termion::cursor::Goto(1, 1), match selected {
+	write!(context.stdout, "{}{}", termion::cursor::Goto(1, 1), match selected {
 		BEGIN => "Hello, Welcome to RAHSCS Terminal Game!",
 		FLOOR => floor_msg,
 		DOOR => {
@@ -126,26 +136,27 @@ fn redraw(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>,
 		},
 		_ => &stringy,
 	}).unwrap();
-	write!(stdout, "{}{}", termion::cursor::Goto(pos.0, pos.1 + 1), PLAYER)
-		.unwrap();
+	write!(context.stdout, "{}{}", termion::cursor::Goto(pos.0, pos.1 + 1),
+		PLAYER).unwrap();
 	found_it
 }
 
 fn main() {
-	let mut world = from_utf8(include_bytes!("levela.text")).unwrap().as_bytes().to_owned();
+	let mut context = Context {
+		world: from_utf8(include_bytes!("levela.text")).unwrap()
+			.as_bytes().to_owned(),
+		stdout: stdout().into_raw_mode().unwrap(),
+	};
+	let mut pos = (1u16, 2u16);
 	// Get the standard input stream.
 	let stdin = stdin();
-	// Get the standard output stream and go to raw mode.
-	let mut stdout = stdout().into_raw_mode().unwrap();
-	//
-	let mut pos : (u16, u16) = (1, 2);
 
-	write!(stdout, "{}{}", termion::clear::All, termion::cursor::Hide)
-		.unwrap();
-	redraw(&mut stdout, &mut world, &mut pos, Direction::Left, 0);
+	write!(context.stdout, "{}{}", termion::clear::All,
+		termion::cursor::Hide).unwrap();
+	redraw(&mut context, &mut pos, Direction::Left, 0);
 
 	// Update terminal screen.
-	stdout.flush().unwrap();
+	context.stdout.flush().unwrap();
 
 	// Main loop
 	for c in stdin.keys() {
@@ -155,53 +166,56 @@ fn main() {
 			Key::Char('f') => {
 				let mut tmppos : (u16, u16);
 				tmppos = (pos.0 - 1, pos.1);
-				let a = redraw(&mut stdout, &mut world,
+				let mut a = redraw(&mut context,
 					&mut tmppos, Direction::Left, 1);
 				if a == false {
 				tmppos = (pos.0 + 1, pos.1);
-				let a = redraw(&mut stdout, &mut world,
+				a = redraw(&mut context,
 					&mut tmppos, Direction::Right, 1);
 				if a == false {
 				tmppos = (pos.0, pos.1 - 1);
-				let a = redraw(&mut stdout, &mut world,
+				a = redraw(&mut context,
 					&mut tmppos, Direction::Up, 1);
 				if a == false {
 				tmppos = (pos.0, pos.1 + 1);
-				redraw(&mut stdout, &mut world,
+				a = redraw(&mut context,
 					&mut tmppos, Direction::Down, 1);
 				}
 				}
 				}
-				redraw(&mut stdout, &mut world, &mut pos,
-					Direction::Left, 2);
+				if a {
+					redraw(&mut context, &mut pos,
+						Direction::Left, 2);
+				}else {
+					redraw(&mut context, &mut pos,
+						Direction::Left, 3);
+				}
 			},
-//			Key::Alt(c)    => println!("Alt-{}", c),
-//			Key::Ctrl(c)   => println!("Ctrl-{}", c),
 			Key::Left      => {
 				if pos.0 > 1 {
 					pos.0 -= 1;
-					redraw(&mut stdout, &mut world, &mut pos,
+					redraw(&mut context, &mut pos,
 						Direction::Left, 0);
 				}
 			},
 			Key::Right     => {
 				if pos.0 < 32 {
 					pos.0 += 1;
-					redraw(&mut stdout, &mut world, &mut pos,
+					redraw(&mut context, &mut pos,
 						Direction::Right, 0);
 				}
 			},
 			Key::Up        => {
 				if pos.1 > 2 {
 					pos.1 -= 1;
-					redraw(&mut stdout, &mut world, &mut pos,
+					redraw(&mut context, &mut pos,
 						Direction::Up, 0);
 				}
 			},
 			Key::Down      => {
 				if pos.1 < 9 {
 					pos.1 += 1;
-					redraw(&mut stdout, &mut world, &mut pos,
+					redraw(&mut context, &mut pos,
 						Direction::Down, 0);
 				}
 			},
@@ -209,10 +223,10 @@ fn main() {
 		}
 
 		// Update terminal screen.
-		stdout.flush().unwrap();
+		context.stdout.flush().unwrap();
 	}
 
 	// Unhide Cursor
-	write!(stdout, "{}{}{}", termion::clear::All,
+	write!(context.stdout, "{}{}{}", termion::clear::All,
 		termion::cursor::Goto(1, 1), termion::cursor::Show).unwrap();
 }
